b84658893fc90638f676d16ee510f3ccd50cb29c
Matt Blackwell <mattblackwell@Matts-MacBook-Pro.local>
2013-01-15 10:44:06 -0600

    Refinery CMS

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RefineryGuestdoor::Application.config.secret_token = '9f529e62119444a2ef5d9962f9919b09fc033dd2c0cead91dea84dd37bffb451ce62e33ce033e6ddf5f3e807cfac7c9ac42caaa0850c5320f15d8ebe13aabb83'
